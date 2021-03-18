bae04fb4c7b5403ae9d2bd2f56bbd3897a1b2119
HP <HP@DESKTOP-L89B3LR>
2021-03-18 11:18:08 +0300

    Bird

---
+++ Bird/src/Bird.java
@@ -0,0 +1,27 @@
+
+public class Bird {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String color= yellow;
+
+public void fly() {
+	System.out.println(" He is bird");
+}
+public void tweet() {
+	System.out.println("The bird's voice is great");
+}
+public class Parrot extends Bird{
+	
+	public void talk() {
+		System.out.println("I am a programmer");
+	}
+	public static void main (String----{
+		parrot p = new parrot();
+		System.out.println("yellow color "+ name);
+		
+	}
+
+	
+
+
